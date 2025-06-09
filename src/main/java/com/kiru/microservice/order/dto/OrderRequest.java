c06836aab6664be208b11871e8a63c000fbba077
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-06-09 19:45:11 +0300

    feat: add OrderRequest DTO to encapsulate order data

---
+++ src/main/java/com/kiru/microservice/order/dto/OrderRequest.java
@@ -0,0 +1,6 @@
+package com.kiru.microservice.order.dto;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+}
