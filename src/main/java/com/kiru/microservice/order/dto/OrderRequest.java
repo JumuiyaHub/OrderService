983e2c25dc0998e212d0a587f6bfee35d3779e25
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-07-08 22:14:20 +0300

    refactor(dto): reorder OrderRequest fields and temporarily remove validation annotations

--- src/main/java/com/kiru/microservice/order/dto/OrderRequest.java
+++ src/main/java/com/kiru/microservice/order/dto/OrderRequest.java
@@ -5,10 +5,12 @@ import java.math.BigDecimal;
 
 public record OrderRequest(
         Long id,
+        String skuCode,
+        Integer quantity,
         String orderNumber,
-        @NotNull(message = "SKU Code must not be null") String skuCode,
+//        @NotNull(message = "SKU Code must not be null") String skuCode,
         BigDecimal price,
-        @NotNull(message = "Quantity must not be null") Integer quantity,
+//        @NotNull(message = "Quantity must not be null") Integer quantity,
         UserDetails userDetails) {
 
     public record UserDetails(String email, String firstName, String lastName) {
